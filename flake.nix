c1d9c62f0117e8c47d14f0db4b1d5b5ba36ec580
Hovirix <184756415+Hovirix@users.noreply.github.com>
2025-12-27 20:29:30 +0100

    build: add Rust package build configuration to flake

--- flake.nix
+++ flake.nix
@@ -12,6 +12,22 @@
         pkgs = import nixpkgs { inherit system; };
       in
       {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "neix";
+          version = "0.0.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          buildInputs = [ pkgs.sqlite ];
+
+          meta = with pkgs.lib; {
+            description = "Blazing fast eix-like search tool for nixpkgs";
+            homepage = "https://github.com/Hovirix/neix";
+            maintainers = with maintainers; [ Hovirix ];
+            license = licenses.mit;
+          };
+        };
+
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             cargo
